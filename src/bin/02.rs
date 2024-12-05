9528da9d4d716a5db68d0113a85e98f4c885bb57
Michael Y <michael.yockney@gmail.com>
2024-12-05 15:32:32 +0700

    day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -17,8 +17,22 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let test = "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9";
+
+    let mut safe = 0;
+
+    for line in input.lines() {
+        let report: Vec<u32> = line.split_whitespace().filter_map(|n| n.parse().ok()).collect();
+
+        if is_safe(&report) {
+            safe += 1;
+            continue;
+        }
+
+        dampen(&report, &mut safe);
+    }
     
-    None
+    Some(safe)
 }
 
 fn is_safe(report: &[u32]) -> bool {
@@ -26,6 +40,20 @@ fn is_safe(report: &[u32]) -> bool {
     let safe_decrease = report.windows(2).all(|lvls| (lvls[0] > lvls[1]) && (lvls[0] - lvls[1] <= 3));
 
     safe_decrease || safe_increase
+}
+
+fn dampen(report: &[u32], safe: &mut u32) {
+    let mut report = report.to_vec();
+    for (i, _) in report.clone().iter().enumerate() {
+        let removed = report.remove(i);
+        
+        if is_safe(&report) {
+            *safe += 1;
+            report.insert(i, removed);
+            return
+        }
+        report.insert(i, removed);
+    }
 }
 
 #[cfg(test)]
